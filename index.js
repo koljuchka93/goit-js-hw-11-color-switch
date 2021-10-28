861f56613dce14c3f1fe8876d8bdaab3968ed080
Kateryna <koljuchka93@gmail.com>
2021-10-29 01:43:23 +0300

    js

--- index.js
+++ index.js
@@ -7,9 +7,30 @@ const colors = [
   '#795548',
 ];
 
-
-
+const refs = {
+    body: document.body,
+    startBtn: document.querySelector('button[data-action = "start"]'),
+    stopBtn: document.querySelector('button[data-action = "stop"]'),
+}
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+let colorId = null;
+
+
+refs.startBtn.addEventListener('click', onStartChange)
+refs.stopBtn.addEventListener('click', onStopChange)
+
+function onStartChange() {
+    colorId = setInterval(() => {
+        refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+    refs.startBtn.disabled = true;
+};
+
+function onStopChange() {
+    clearInterval(colorId);
+        refs.startBtn.disabled = false;
+
+}
